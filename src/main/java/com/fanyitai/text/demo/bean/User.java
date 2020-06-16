5760c5f3c0fe9c08892c8dc27dd7e11b2f5f9e8a
fanyitai <1296603386@qq.com>
2020-06-16 16:24:56 +0800

    建立仓库

---
+++ src/main/java/com/fanyitai/text/demo/bean/User.java
@@ -0,0 +1,22 @@
+package com.fanyitai.text.demo.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    private Integer id;
+    private String userName;
+    private String userPassword;
+    //注册时间
+    private String registerTime;
+    //上次登陆时间
+    private String loginTime;
+}
